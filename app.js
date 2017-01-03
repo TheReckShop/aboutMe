57e5e7325796f3d8fe5177e79fd160d01544e065
thereckshop <thereckshop@mac.com>
2017-01-03 14:45:24 -0800

    added js file structure

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var greeting = confirm('Hello, I\'m David C Cole\'s aboutMe Bot! We are going to play a guessing game? All questions have yes or no answers. Are you ready?');
+
+if (greeting) {
+  console.log('Woot! You straight prepared holmes!');
+} else {
+  console.log('OH NO! AHHHHHHHHHHHHH');
+}
+
+var
