5dfb5ebb8fc7a2cbd3a149882bc4632ea5914a01
thereckshop <thereckshop@mac.com>
2017-01-03 15:11:55 -0800

    added first question

--- app.js
+++ app.js
@@ -8,4 +8,14 @@ if (greeting) {
   console.log('OH NO! AHHHHHHHHHHHHH');
 }
 
-var
+var user = prompt('What is your name Bruh?');
+console.log('This user\'s name is ' + user + '!');
+
+var q1 = prompt('So first question: Does David own two cats?').toUpperCase();
+if (q1 === 'Yes' || 'Y' || 'Ya') {
+  alert('OMG! You are TOTES RIGHT!');
+  console.log('They got q1 right!');
+} else {
+  alert('WA WA WA WAAAAAAA! You got it wrong. He has two kitties!');
+  console.log('They got q1 wrong!');
+}
