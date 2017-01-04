c2fe180872407a7bd5d6d2023edd037e970fd27f
thereckshop <thereckshop@mac.com>
2017-01-04 07:00:55 -0800

    finished app.js

--- app.js
+++ app.js
@@ -17,11 +17,11 @@ document.write('So first question ' + user + ': Does David own two cats?');
 if (q1 === 'Yes' || q1 === 'Y' || q1 === 'Ya') {
   alert('OMG! You are TOTES RIGHT!');
   console.log('They got q1 right!');
-  document.write('RIGHT!');
+  document.write('q1 RIGHT!');
 } else {
   alert('WA WA WA WAAAAAAA! You got it wrong. He has two kitties!');
   console.log('They got q1 wrong!');
-  document.write('WRONG!');
+  document.write('q1 WRONG!');
 }
 
 var q2 = prompt('Second question ' + user + ': Does David manage a night club on the weekends?').toUpperCase();
@@ -29,34 +29,45 @@ document.write('Second question ' + user + ': Does David manage a night club on 
 if (q2 === 'Yes' || q2 === 'Y' || q2 === 'yes') {
   alert('CORRECT! You\'re a smart cookie!');
   console.log('They got q2 right!');
+  document.write('You got q2 right!');
 } else {
   alert('WA WA WA WAAAAAA! You got it wrong. He does manage a nightclub on the weekends. He hates mondays');
   console.log('They got q2 wrong!');
+  document.write('You got q2 wrong!');
 }
 
 var q3 = prompt('Third question ' + user + ': Is David going to the Seahawks Playoff game this weekend?').toUpperCase();
+document.write('Third question ' + user + ': Is David going to the Seahawks Playoff game this weekend?');
 if (q3 === 'Yes' || q3 === 'Y' || q3 === 'ya') {
   alert('YUUUUUUUPPPPPP! You\'re a smart cookie!');
   console.log('They got q3 right!');
+  document.write('q3 RIGHT!');
 } else {
   alert('WA WA WA WAAAAAA! You got it wrong. GO HAWKS!');
   console.log('They got q3 wrong!');
+  document.write('q3 WRONG!');
 }
 
 var q4 = prompt('fourth question ' + user + ': Does David love the Portland Timbers?').toUpperCase();
+document.write('fourth question ' + user + ': Does David love the Portland Timbers?');
 if (q4 === 'No' || q4 === 'N' || q4 === 'na') {
   alert('SWEEEEEETTTT! You know me so well!');
   console.log('They got q4 right!');
+  document.write('q4 RIGHT1');
 } else {
   alert('WA WA WA WAAAAAA! You got it wrong. I thought we went way back bruv!');
   console.log('They got q4 wrong!');
+  document.write('q4 WRONG');
 }
 
 var q5 = prompt('Fifth question ' + user + ': Does David think codefellows is stupid?').toUpperCase();
+document.write('Fifth question ' + user + ': Does David think codefellows is stupid?');
 if (q5 === 'No' || q5 === 'N' || q5 === 'na') {
   alert('GOOD JOB! Now we\'re besties!');
   console.log('They got q5 right!');
+  document.write('q5 RIGHT!');
 } else {
   alert('WA WA WA WAAAAAA! You got it wrong. Codefellows is great!');
   console.log('They got q5 wrong!');
+  document.write('q5 WRONG!');
 }
