ae36434fa08cd74b210cc8a1edd5170a63cccff9
thereckshop <thereckshop@mac.com>
2017-01-04 14:13:05 -0800

    working on question 6

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ var user = prompt('What is your name Bruh?');
 console.log('This user\'s name is ' + user + '!');
 document.write('User name is: ' + user);
 
+var rightAnswer = 0;
+
 var q1 = prompt('So first question ' + user + ': Does David own two cats?').toUpperCase();
 document.write('So first question ' + user + ': Does David own two cats?');
 
@@ -19,6 +21,8 @@ if (q1 === 'YES' || q1 === 'Y' || q1 === 'YA') {
   alert('OMG! You are TOTES RIGHT!');
   console.log('They got q1 right!');
   document.write('q1 RIGHT!');
+  rightAnswer ++;
+  console.log('Answers correct = ' + rightAnswer);
 } else {
   alert('WA WA WA WAAAAAAA! You got it wrong. He has two kitties!');
   console.log('They got q1 wrong!');
@@ -32,6 +36,8 @@ if (q2 === 'YES' || q2 === 'Y' || q2 === 'YA') {
   alert('CORRECT! You\'re a smart cookie!');
   console.log('They got q2 right!');
   document.write('You got q2 right!');
+  rightAnswer++;
+  console.log('Answers correct = ' + rightAnswer);
 } else {
   alert('WA WA WA WAAAAAA! You got it wrong. He does manage a nightclub on the weekends. He hates mondays');
   console.log('They got q2 wrong!');
@@ -45,6 +51,8 @@ if (q3 === 'YES' || q3 === 'Y' || q3 === 'YA') {
   alert('YUUUUUUUPPPPPP! You\'re a smart cookie!');
   console.log('They got q3 right!');
   document.write('q3 RIGHT!');
+  rightAnswer++;
+  console.log('Answers correct = ' + rightAnswer);
 } else {
   alert('WA WA WA WAAAAAA! You got it wrong. GO HAWKS!');
   console.log('They got q3 wrong!');
@@ -58,6 +66,8 @@ if (q4 === 'NO' || q4 === 'N' || q4 === 'NA') {
   alert('SWEEEEEETTTT! You know me so well!');
   console.log('They got q4 right!');
   document.write('q4 RIGHT1');
+  rightAnswer++;
+  console.log('Answers correct = ' + rightAnswer);
 } else {
   alert('WA WA WA WAAAAAA! You got it wrong. I thought we went way back bruv!');
   console.log('They got q4 wrong!');
@@ -71,8 +81,20 @@ if (q5 === 'NO' || q5 === 'N' || q5 === 'NA') {
   alert('GOOD JOB! Now we\'re besties!');
   console.log('They got q5 right!');
   document.write('q5 RIGHT!');
+  rightAnswer++;
+  console.log('Answers right = ' + rightAnswer);
 } else {
   alert('WA WA WA WAAAAAA! You got it wrong. Codefellows is great!');
   console.log('They got q5 wrong!');
   document.write('q5 WRONG!');
+}
+
+var q6Answer = parseInt(prompt('Sixth question ' + user + ': What is David\'s favorite number?'));
+document.write('Sixth question ' + user + ': What is David\'s favorite number?');
+
+var q6Guesses = 0;
+while (q6Guesses < 4) {
+  for (q6Answer === 7) {
+    alert('Well done! My favorite number is ')
+  }
 }
