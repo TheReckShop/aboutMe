4bb47dd1137fde20f6459aa518a4656d92a94df6
thereckshop <thereckshop@mac.com>
2017-01-04 09:33:47 -0800

    blah

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var greeting = confirm('Hello, I\'m David C Cole\'s aboutMe Bot! We are going to play a guessing game? All questions have yes or no answers. Are you ready?');
+var greeting = confirm('Hello, I\'m David C Cole\'s aboutMe Bot! We are going to play a guessing game! All questions have yes or no answers. Are you ready?');
 
 if (greeting) {
   console.log('Woot! You straight prepared holmes!');
